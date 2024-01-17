4832618dd1e6b86c68ab48084340d7caed8bf4f7
Robert Lutz <robertlutzi@gmail.com>
2024-01-16 21:45:42 -0500

    add function that attempts to find all zeros

---
+++ newton.rkt
@@ -0,0 +1,86 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname newton) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+
+
+(define (derivative coeff-list)
+  ; [ListOf N] -> [ListOf N]
+  ; takes the coefficients for a polynomial and returns
+  ; the coefficients of that polynomial's derivative
+  (rest (map * coeff-list (build-list (length coeff-list) identity))))
+
+
+(define (make-func coefficients)
+  ; [ListOf N] -> [Number -> Number]
+  ; takes as input a list of corfficients and returns a function
+  ; that describes the polynomial with those coefficients
+  (lambda (x) 
+    (foldr
+     (λ (c xn rem) (+ (* c xn) rem))
+     #i0
+     coefficients
+     (build-list (length coefficients) (λ (n) (expt x n))))))
+
+
+(define (newton coeffs guess)
+  ; [ListOf Number] Number -> [Maybe Number]
+  ; takes as an argument a list of integer coefficients which gets
+  ; interpreted as the coefficients of a polynomial function, as well as
+  ; an initial guess at one of the roots
+  ; It then finds the nearest root to guess using Newton's Method
+  (local (
+          (define tol 1e-6)
+          (define f (make-func coefficients))
+          (define df (make-func (derivative coefficients)))          
+          (define (method root i)
+            ; Number N -> [Maybe Number]
+            ; using a recursive approach, homes in on the nearest root
+            ; using Newton's Method, stopping once f(root) is
+            ; sufficiently close to 0
+            (cond
+              [(> i 128) #f]
+              [(< (abs (f root)) tol) (inexact->exact root)]
+              [else (method (- root (/ (f root) (df root))) (add1 i))])))
+    ; - IN -
+    (method guess 0)))
+     
+            
+(define (find-all-zeros coeffs)
+  ; [ListOf N] -> [ListOf Number]
+  ; A best attempt to return all the roots of a given polynomial
+  (local (
+          (define range 5)
+          (define raw-range
+            (build-list (+ (* 2 range) 1) (lambda (n) (expt 2 (- n range)))))
+          (define starting-points
+            (append (map (λ (r) (- r)) raw-range) raw-range))
+          (define raw-results (map (λ (g) (newton coeffs g)) starting-points))
+          (define approx-solutions
+            (filter (λ (r) (not (false? r))) raw-results))
+          (define solutions
+            (map (λ (s) (/ (round (* 1e6 s)) 1e6)) approx-solutions)))
+    ; - IN -
+    (sort (settle solutions) <)))
+
+
+(define (settle lst)
+  ; [ListOf X] -> [ListOf X]
+  ; converts a list into a set
+  (cond
+    [(empty? lst) '()]
+    [(member? (first lst) (rest lst)) (settle (rest lst))]
+    [else (cons (first lst) (settle (rest lst)))]))
+
+
+
+; ==========================
+; action!
+
+(define order 8)
+(define coefficients (build-list 8 (λ (n) (- (random 201) 100))))
+;(define coefficients (list 67 -98 2 -38 46 0 42 2)) ; hard case here
+;(define coefficients (list -24 -5 -88 34 86 -59 -1 -24)) ; another one
+;(define coefficients (list -27 1 84 86 -38 -94 -9 -84)) ; again
+coefficients
+(find-all-zeros coefficients)
